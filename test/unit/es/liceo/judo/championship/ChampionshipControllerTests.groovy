410626e88f8fe998a1f83801cc87e94909628368
Pablo Vazquez <pablovb@gmail.com>
2011-03-27 23:55:19 +0200

    a

---
+++ test/unit/es/liceo/judo/championship/ChampionshipControllerTests.groovy
@@ -0,0 +1,17 @@
+package es.liceo.judo.championship
+
+import grails.test.*
+
+class ChampionshipControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/es/liceo/judo/championship/ChampionshipTests.groovy
@@ -0,0 +1,17 @@
+package es.liceo.judo.championship
+
+import grails.test.*
+
+class ChampionshipTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
